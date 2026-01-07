e4876b500d12a3d131541387215f6ab9fa766eeb
Devanshu Joshi <iamdevanshu007@gmail.com>
2026-01-07 19:36:34 +0530

    feat(dashboard): update dashboard layout with login, signup, and logout buttons

--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { AuthRoutingModule } from "../../../auth/auth-routing-module";
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../../core/services/auth';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ButtonModule, AuthRoutingModule, RouterModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
 
+  constructor(private auth: AuthService) { }
+  logout() {
+    this.auth.logout();
+  }
 }
